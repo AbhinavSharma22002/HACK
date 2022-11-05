4a3ada95836f9273706a986bc1b56a01565b00a4
akshat jain <akshat.2024it1031@kiet.edu>
2022-11-06 04:37:28 +0530

    pushing-snap

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,22 +1,21 @@
-$(document).ready(function()
-{
-  if(localStorage.getItem("token")!= null)
-{
-  $("#register").css("display","none")
-}
-else
-{
-  
-  $("#register").css("display","block")
-}
+$(document).ready(function () {
+  if (localStorage.getItem("token") != null) {
+    $("#register").css("display", "none");
+    const webcamElement = document.getElementById("webcam");
+    const webcam = new Webcam(webcamElement, "user");
+    webcam
+      .start()
+      .then((result) => {
+        console.log("webcam started");
+        let picture = webcam.snap();
+        document.querySelector("#download-photo").href = picture;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    $("#register").css("display", "block");
+  }
 });
 
-
-navigator.mediaDevices.getUserMedia({
-  video:true
-}).then((stream)=>{
- console.log(stream);
-      // Some more code
-}).catch(err=>{
-    alert(err.message)
-})
+//
